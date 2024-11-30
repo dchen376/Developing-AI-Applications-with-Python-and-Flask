cd57c5a3ef23dc869089a4aeffda6ecb5967cda0
dongping c <122350071+dchen376@users.noreply.github.com>
2024-11-29 23:02:56 -0800

    Web Deployment of the application package
    Task 1: Create the mathematical functions.
    Task 2: Package the functions and test the package.
    Task 3: Web Deployment of the application package using Flask.

---
+++ hjbsk-build_deploy_app_flask/Maths/__init__.py
@@ -0,0 +1,6 @@
+# Import the file mathematics.py to the __init__.py file.
+from . import mathematics
+
+# Import the package Maths in server.py.
+from Maths.mathematics import summation, subtraction, multiplication
+
---
+++ hjbsk-build_deploy_app_flask/Maths/mathematics.py
@@ -0,0 +1,19 @@
+```python
+def summation(a, b):
+    result = a + b
+    return result
+```
+```python
+def subtraction(a, b):
+    result = a - b
+    return result
+```
+
+```python
+def multiplication(a, b):
+    result = a * b
+    return result
+```
+
+
+
